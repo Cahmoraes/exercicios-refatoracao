3763f92c8c3e93fadcd3868d7cc0ef4926aca474
caique.moraes <caique.moraes@compasso.com.br>
2022-11-10 11:39:42 -0300

    adicionado função amountFor

--- src/index.js
+++ src/index.js
@@ -5,33 +5,46 @@ function statement(invoice, plays) {
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`
+
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
-    let thisAmount = 0
+    let thisAmount = amountFor(perf, play)
+
+    volumeCredits += Math.max(perf.audience - 30, 0)
+
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5)
+
+    result += `${play.name}: ${thisAmount / 100} (${perf.audience} seats)\n`
+
+    totalAmount += thisAmount
+  }
+
+  result += `Amount owed is ${totalAmount / 100}\n`
+  result += `You earned ${volumeCredits} credits\n`
+  return result
+
+  function amountFor(perf, play) {
+    let result = 0
+
     switch (play.type) {
       case 'tragedy':
-        thisAmount = 40000
+        result = 40000
         if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
+          result += 1000 * (perf.audience - 30)
         }
         break
       case 'comedy':
-        thisAmount = 30000
+        result = 30000
         if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20)
+          result += 10000 + 500 * (perf.audience - 20)
         }
         break
       default:
         throw new Error(`unknown type: ${play.type}`)
     }
-    volumeCredits += Math.max(perf.audience - 30, 0)
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5)
-    result += `${play.name}: ${thisAmount / 100} (${perf.audience} seats)\n`
-    totalAmount += thisAmount
+
+    return result
   }
-  result += `Amount owed is ${totalAmount / 100}\n`
-  result += `You earned ${volumeCredits} credits\n`
-  return result
 }
 
 const result = statement(invoices, plays)
