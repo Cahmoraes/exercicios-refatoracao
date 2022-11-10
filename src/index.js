cd58a0877d80159e617ba3a177282c0a7dee1da0
caique.moraes <caique.moraes@compasso.com.br>
2022-11-10 16:49:05 -0300

    removido invoices da declaração de renderPlainText

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import invoices from './seeders/invoices.json' assert { type: 'json' }
 function statement(invoice, plays) {
   const statementData = {}
   statementData.costumer = invoice.costumer
-  return renderPlainText(statementData, invoice, plays)
+  statementData.performances = invoices.performances
+
+  return renderPlainText(statementData, plays)
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`
@@ -23,7 +25,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmount() {
     let result = 0
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf)
     }
     return result
@@ -31,7 +33,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result = volumeCreditFor(perf)
     }
     return result
