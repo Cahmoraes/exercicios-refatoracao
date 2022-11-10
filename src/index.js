6dcb74e26d4810f59c24e2ec71cb3748f7a36586
caique.moraes <caique.moraes@compasso.com.br>
2022-11-10 15:25:59 -0300

    extração de format para uma função

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import invoices from './seeders/invoices.json' assert { type: 'json' }
 
 function statement(invoice, plays) {
   let totalAmount = 0
-  let result = `Statement for ${invoice.customer}\n`
   let volumeCredits = 0
+  let result = `Statement for ${invoice.customer}\n`
 
   for (let perf of invoice.performances) {
     volumeCredits = volumeCreditFor(perf)
 
-    result += `${playFor(perf).name}: ${amountFor(perf) / 100} (${
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`
 
     totalAmount += amountFor(perf)
   }
 
-  result += `Amount owed is ${totalAmount / 100}\n`
+  result += `Amount owed is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
 
   return result
@@ -54,6 +54,13 @@ function statement(invoice, plays) {
     if ('comedy' === playFor(aPerformance).type) {
       result += Math.floor(aPerformance.audience / 5)
     }
+  }
+
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(aNumber)
   }
 }
 
